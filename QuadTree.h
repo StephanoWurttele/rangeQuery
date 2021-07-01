53e395b0ca133b120b46815d1baf701c9adb5efe
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2021-06-30 19:42:21 -0500

    wtf

--- QuadTree.h
+++ QuadTree.h
@@ -22,14 +22,13 @@ private:
      * @return Retorna referencia al Nodo que contiene o podría contener el punto buscado
      */
     std::shared_ptr<Node>& search(Point target, std::shared_ptr<Node>& node);
-    bool containsPoint(Point target, Rectangle region);
 
 public:
     QuadTree();
     void insert(Point new_point) override;
     std::shared_ptr<Node> search(Point target) override;
     std::vector<Point> range(Rectangle region) override;
-    std::vector<Point> range_recursive(std::shared_ptr<Node>& root, Rectangle region);
+    std::vector<Point> range_recursive(Rectangle region) override;
     Point nearest_neighbor(Point reference_point) override;
 };
 
